47b7a014786e14d3f724bc950024da68b8a6890d
Anthony Suárez <neolight1010@gmail.com>
2025-06-29 20:39:33 -0500

    Start defining Ledger type and related IO

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,9 +1,43 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DuplicateRecordFields #-}
+
 module Lib
   ( Item (..),
     Expenses (..),
     expensesTotal,
+    Ledger (..),
+    ExpenseList,
+    ExpenseEntry (..),
+    ExpenseGroups,
   )
 where
+
+import Data.Map (Map)
+import Data.Yaml.Aeson (FromJSON)
+import GHC.Generics (Generic)
+
+newtype Ledger = Ledger
+  { expenses :: ExpenseGroups
+  }
+  deriving (Generic, Eq, Show)
+
+instance FromJSON Ledger
+
+type ExpenseGroups = Map String ExpenseList
+
+type ExpenseList = [ExpenseEntry]
+
+data ExpenseEntry = ExpenseEntry
+  { item :: ItemId,
+    price :: Price
+  }
+  deriving (Generic, Eq, Show)
+
+instance FromJSON ExpenseEntry
+
+type ItemId = String
+
+type Price = Float
 
 newtype Item = Item String
   deriving (Show)
@@ -12,4 +46,4 @@ newtype Expenses = Expenses [(Item, Float)]
   deriving (Show)
 
 expensesTotal :: Expenses -> Float
-expensesTotal (Expenses expenses) = sum $ map snd expenses
+expensesTotal (Expenses xs) = sum $ map snd xs
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,12 @@
-import Lib (Expenses (..), Item (..), expensesTotal)
-import Test.HUnit (Test (TestCase, TestLabel, TestList), runTestTT, (@?=), (~?=))
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE PartialTypeSignatures #-}
+
+import Data.Either (isLeft)
+import Data.Map (fromList)
+import Data.Yaml (decodeEither')
+import Data.Yaml.Internal (ParseException)
+import Lib (ExpenseEntry (..), ExpenseGroups (..), Expenses (..), Item (..), Ledger (..), expensesTotal)
+import Test.HUnit (Test (TestLabel, TestList), runTestTT, (~?), (~?=))
 
 main :: IO ()
 main = do
@@ -7,11 +14,41 @@ main = do
     runTestTT $
       TestLabel "lib tests" $
         TestList
-          [ expensesTotal (Expenses [])
-              ~?= 0,
-            expensesTotal
-              (Expenses [(Item "item", 1)])
-              ~?= 1
+          [ TestLabel "expensesTotal" $
+              TestList
+                [ expensesTotal (Expenses [])
+                    ~?= 0,
+                  expensesTotal
+                    (Expenses [(Item "item", 1)])
+                    ~?= 1
+                ],
+            TestLabel
+              "ledger io"
+              $ TestList
+                [ let decoded :: Either ParseException Ledger
+                      decoded = decodeEither' ""
+                   in isLeft decoded ~? "expected decoding to fail",
+                  let decoded :: Either ParseException Ledger
+                      decoded =
+                        decodeEither'
+                          "expenses:\n\
+                          \  group1:\n\
+                          \    - item: item1\n\
+                          \      price: 0.1\n\
+                          \"
+                   in case decoded of
+                        Left e -> False ~? "decoding should not have failed: " ++ show e
+                        Right ledger ->
+                          ledger
+                            ~?= Ledger
+                              ( fromList
+                                  [ ( "group1",
+                                      [ ExpenseEntry {item = "item1", price = 0.1}
+                                      ]
+                                    )
+                                  ]
+                              )
+                ]
           ]
 
   pure ()
