59879a4ed95bd3c274ddf6cf3ebc9f4be91b62c4
Anthony Suárez <neolight1010@gmail.com>
2025-07-01 21:23:35 -0500

    Set up initial CLI

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,30 @@
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
 module Main (main) where
 
+import Control.Monad.IO.Class (MonadIO (..))
+import qualified Iris
+
+newtype App a = App
+  { unApp :: Iris.CliApp () () a
+  }
+  deriving newtype
+    ( Functor,
+      Applicative,
+      Monad,
+      MonadIO
+    )
+
+appSettings :: Iris.CliEnvSettings () ()
+appSettings =
+  Iris.defaultCliEnvSettings
+    { Iris.cliEnvSettingsHeaderDesc = "Expense Tracker",
+      Iris.cliEnvSettingsProgDesc = "A minimal, file-based expense tracker app."
+    }
+
+app :: App ()
+app = liftIO $ putStrLn "Hello, World!"
+
 main :: IO ()
-main = putStrLn "Hello, World!"
+main = Iris.runCliApp appSettings $ unApp app
