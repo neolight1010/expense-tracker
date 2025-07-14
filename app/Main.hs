038651de7f95875c935860c1b6418cf17b9ea4a8
Anthony Suárez <neolight1010@gmail.com>
2025-07-13 20:51:36 -0500

    Define placeholder 'list' command

--- app/Main.hs
+++ app/Main.hs
@@ -1,30 +1,51 @@
 {-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE NamedFieldPuns #-}
 
 module Main (main) where
 
 import Control.Monad.IO.Class (MonadIO (..))
 import qualified Iris
+import qualified Options.Applicative as Opt
+import Control.Monad.Reader (MonadReader)
 
 newtype App a = App
-  { unApp :: Iris.CliApp () () a
+  { unApp :: Iris.CliApp Options () a
   }
   deriving newtype
     ( Functor,
       Applicative,
       Monad,
-      MonadIO
+      MonadIO,
+      MonadReader (Iris.CliEnv Options ())
     )
 
-appSettings :: Iris.CliEnvSettings () ()
+appSettings :: Iris.CliEnvSettings Options ()
 appSettings =
   Iris.defaultCliEnvSettings
     { Iris.cliEnvSettingsHeaderDesc = "Expense Tracker",
-      Iris.cliEnvSettingsProgDesc = "A minimal, file-based expense tracker app."
+      Iris.cliEnvSettingsProgDesc = "A minimal, file-based expense tracker app.",
+      Iris.cliEnvSettingsCmdParser = cmdParser
     }
 
+cmdParser :: Opt.Parser Options
+cmdParser =
+  Options <$> Opt.hsubparser
+    ( Opt.command "list" (Opt.info (pure List) (Opt.progDesc "List all the expense groups"))
+    )
+
+newtype Options = Options {
+  optCommand :: Command
+}
+
+data Command = List
+  deriving (Show)
+
 app :: App ()
-app = liftIO $ putStrLn "Hello, World!"
+app = do
+  Options {optCommand} <- Iris.asksCliEnv Iris.cliEnvCmd
+
+  liftIO $ print optCommand
 
 main :: IO ()
 main = Iris.runCliApp appSettings $ unApp app
