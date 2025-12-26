b8f827074123daaf218a5c375f6894637b688fe1
Anthony Suárez <neolight1010@gmail.com>
2025-12-25 22:09:38 -0500

    Improve display of summary

--- app/ExpensesCli/SummaryCommand.hs
+++ app/ExpensesCli/SummaryCommand.hs
@@ -1,14 +1,46 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module ExpensesCli.SummaryCommand (handleSummaryCommand) where
 
 import Data.ByteString.Char8 (unpack)
-import Data.Yaml (encode)
-import Lib.Ledger (Ledger, expenseGroupSummary)
-import Lib.ExpenseGroup (ExpenseGroupSummary)
-import qualified Rainbow
+import Data.Function ((&))
+import Data.List (intersperse)
 import Data.String (fromString)
+import Data.Yaml (encode)
+import Lib.ExpenseGroup (ExpenseGroupSummary (categoryTotals, itemTotals, total))
+import Lib.Ledger (Ledger, expenseGroupSummary)
+import qualified Rainbow
 
 handleSummaryCommand :: Ledger -> String -> IO ()
-handleSummaryCommand ledger = Rainbow.putChunkLn . showSummary . expenseGroupSummary ledger
+handleSummaryCommand ledger = Rainbow.putChunksLn . showSummary . expenseGroupSummary ledger
 
-showSummary :: Maybe ExpenseGroupSummary -> Rainbow.Chunk
-showSummary summary = Rainbow.fore Rainbow.blue (fromString $ unpack $ encode summary)
+showSummary :: Maybe ExpenseGroupSummary -> [Rainbow.Chunk]
+showSummary summary =
+  [ [Rainbow.fore Rainbow.red "Expense Group - Summary\n"],
+    showCategoryTotals summary,
+    showItemTotals summary,
+    showGrandTotal summary
+  ]
+    & (intersperse "\n" . concat)
+
+showCategoryTotals :: Maybe ExpenseGroupSummary -> [Rainbow.Chunk]
+showCategoryTotals summary =
+  let categories :: [Rainbow.Chunk]
+      categories =
+        (\(category, total_) -> Rainbow.bold (fromString (category ++ ": ")) <>  fromString (show total_) <> "\n")
+          <$> concatMap categoryTotals summary
+   in [ Rainbow.fore Rainbow.cyan "Category totals:",
+        Rainbow.fore Rainbow.white (mconcat categories)
+      ]
+
+showItemTotals :: Maybe ExpenseGroupSummary -> [Rainbow.Chunk]
+showItemTotals summary =
+  [ Rainbow.fore Rainbow.cyan "Item totals:",
+    Rainbow.fore Rainbow.white (fromString $ unpack $ encode (itemTotals <$> summary))
+  ]
+
+showGrandTotal :: Maybe ExpenseGroupSummary -> [Rainbow.Chunk]
+showGrandTotal summary =
+  [ Rainbow.fore Rainbow.cyan "Grand total:",
+    Rainbow.fore Rainbow.white (fromString $ unpack $ encode (total <$> summary))
+  ]
