d050e298c6df9a305ebb91fad7b8bca7866c63f7
Anthony Suárez <neolight1010@gmail.com>
2025-12-25 21:01:50 -0500

    Start using Rainbow package

--- app/ExpensesCli/SummaryCommand.hs
+++ app/ExpensesCli/SummaryCommand.hs
@@ -3,6 +3,12 @@ module ExpensesCli.SummaryCommand (handleSummaryCommand) where
 import Data.ByteString.Char8 (unpack)
 import Data.Yaml (encode)
 import Lib.Ledger (Ledger, expenseGroupSummary)
+import Lib.ExpenseGroup (ExpenseGroupSummary)
+import qualified Rainbow
+import Data.String (fromString)
 
 handleSummaryCommand :: Ledger -> String -> IO ()
-handleSummaryCommand ledger = putStrLn . unpack . encode . expenseGroupSummary ledger
+handleSummaryCommand ledger = Rainbow.putChunkLn . showSummary . expenseGroupSummary ledger
+
+showSummary :: Maybe ExpenseGroupSummary -> Rainbow.Chunk
+showSummary summary = Rainbow.fore Rainbow.blue (fromString $ unpack $ encode summary)
